da81fd262b77002a1c8111eb4178f914ada34cef
douglasmatosdev <douglasmatosdev@gmail.com>
2025-04-21 17:10:08 -0300

    wifi config

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-const SSID = 'RedeBR-Kiara-2.4G'; // Nome da rede WiFi
-const PASSWORD = 'joannamatos09092016';
-const ENCRYPTION = 'WPA'; // Pode ser WEP, WPA ou deixar em branco para redes abertas
+function getDataToCreateQRCode() {
+	const ssid = document.getElementById('ssid').value;
+	const password = document.getElementById('password').value;
+	const encryption = document.getElementById('encryption').value;
 
-document.addEventListener("DOMContentLoaded", function() {
-	var qrCodeText = `WIFI:T:${ENCRYPTION};S:${SSID};P:${PASSWORD};;`;
-	var qrcode = new QRCode("qrcode", {
+	if (!ssid || !password) {
+		alert("SSID e Senha são obrigatórios.");
+		return null;
+	}
+
+	if (encryption === 'None') {
+		return `WIFI:S:${ssid};P:${password};;`;
+	}
+
+	return `WIFI:T:${encryption};S:${ssid};P:${password};;`;
+}
+
+function generateQRCode() {
+	const qrCodeText = getDataToCreateQRCode();
+	if (!qrCodeText) return;
+
+	const qrcode = new QRCode("qrcode", {
 		text: qrCodeText,
-		width: 450,
-		height: 450,
-		colorDark : "#000000",
-		colorLight : "#ffffff",
-		correctLevel : QRCode.CorrectLevel.H
+		width: 380,
+		height: 380,
+		colorDark: "#000000",
+		colorLight: "#ffffff",
+		correctLevel: QRCode.CorrectLevel.H
+	});
+}
+
+function clearFields() {
+	const ssid = document.getElementById('ssid');
+	const password = document.getElementById('password');
+	const encryption = document.getElementById('encryption');
+	const qrCode = document.getElementById('qrcode');
+	ssid.value = '';
+	password.value = '';
+	encryption.value = 'None';
+	qrCode.innerHTML = '';
+	qrCode.style.display = 'none';
+	document.getElementById('qrcode').style.display = 'none';
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	const generateButton = document.getElementById('apply');
+	const clearButton = document.getElementById('clear');
+
+	generateButton?.addEventListener('click', function () {
+		generateQRCode();
+	});
+
+	console.log("Botão de gerar QR Code encontrado:", generateButton);
+	console.log("Botão de limpar encontrado:", clearButton);
+	
+
+	clearButton?.addEventListener('click', function () {
+		clearFields();
 	});
 });
+
